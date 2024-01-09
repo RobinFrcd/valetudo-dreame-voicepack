79af0cf83def43262cbfe8938dd00ecc2a07f3a6
RobinFrcd <29704178+RobinFrcd@users.noreply.github.com>
2024-01-09 16:41:36 +0100

    Add 👸 QueenElizabethII pack

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Tested with Dreame L10s, sound list taken [here](https://github.com/Findus23/voi
 1. `poetry install` to install
 2. `poetry run jupyter lab`
 3. Use the `GenerateVoices.ipynb` notebook with our own `VOICE_ID`, `FY_USER` and `FY_PWD`
-4. Run the notebook to generate the wav files and `./wav2ogg.sh voice_packs/mj/wav mj` to generate the tar.gz
+4. Run the notebook to generate the wav files and `./wav2ogg.sh -i voice_packs/mj/wav -o mj` to generate the tar.gz
 
 You'll need `ffmpeg` and `vorbis-tools` to make the `wav2ogg.sh` script run.
 
@@ -17,3 +17,7 @@ You'll need `ffmpeg` and `vorbis-tools` to make the `wav2ogg.sh` script run.
 - 🕴🏻Michael Jackson:
     - https://github.com/RobinFrcd/valetudo-dreame-voicepack/releases/download/mj/mj.tar.gz
     - MD5: `d00b935eb75c4284814d4200a4ac940e`
+
+- 👸 QueenElizabethII:
+    - https://github.com/RobinFrcd/valetudo-dreame-voicepack/releases/download/QueenElizabethII/Queen_Elizabeth.tar.gz
+    - MD5: `af3df2c355a6a2c6aaca00e76b23bbf0`
--- wav2ogg.sh
+++ wav2ogg.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-# The input directory containing the wav files
-input_dir="$1"
-pack_name="$2"
+# Parse the command line arguments
+convert_flag=false
+while getopts "ci:o:" opt; do
+    case ${opt} in
+        c )
+            convert_flag=true
+            ;;
+        i )
+            input_dir=${OPTARG}
+            ;;
+        o )
+            pack_name=${OPTARG}
+            ;;
+  \? )
+    echo "Invalid Option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+ esac
+done
 
 # The output directory where the ogg files will be saved
 output_dir="${input_dir}/../ogg"
 norm_dir="${input_dir}/../wav-norm"
 
 # Create the output directory if it doesn't exist
+
 rm -rf $output_dir
 rm -rf $norm_dir
 mkdir -p "$output_dir"
 mkdir -p "$norm_dir"
+echo $convert_flag
+echo $input_dir
+echo $pack_name
 
 # Loop through all the wav files in the input directory
 for file in "$input_dir"/*.wav; do
     # Get the base name of the file without the extension
     base_name=$(basename "$file" .wav)
-
-    ffmpeg -i "${file}" -filter:a "loudnorm=I=-5:LRA=1:dual_mono=true:tp=-1, volume=4" "$norm_dir/$base_name.wav"
-    oggenc "$norm_dir/$base_name.wav" --output "$output_dir/$base_name.ogg" --bitrate 100 --resample 16000
+    if [[ $convert_flag == true ]]; then
+        ~/Downloads/ffmpeg -i "${file}" -filter:a "loudnorm=I=-5:LRA=1:dual_mono=true:tp=-1, volume=4" "$norm_dir/$base_name.wav"
+        oggenc "$norm_dir/$base_name.wav" --output "$output_dir/$base_name.ogg" --bitrate 100 --resample 16000
+    else
+        ~/Downloads/ffmpeg -i ${file} -acodec libvorbis -b:a 100k -ar 16000 -af "volume=12dB" "$output_dir/$base_name.ogg"
+    fi
 done
 
 root_dir=$(pwd)
